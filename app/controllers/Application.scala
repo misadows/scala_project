fda4ef1596f1b327abe083ab37da4c72d3198f31
Michał Sadowski <sadowskimichal95@gmail.com>
2016-01-13 16:47:11 +0100

    Database connection

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,19 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import anorm._
+import play.api.db.DB
+import play.api.Play.current
 
 class Application extends Controller {
 
   def index = Action {
-    request => Ok("Got request [" + request + "]")
+    var r:String = ""
+    DB.withConnection { implicit c =>
+      val result: Boolean = SQL("Select 1").execute()
+      r=result.toString;
+    }
+    Ok("Got request [" + r + "]")
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -6,11 +6,14 @@ scalaVersion := "2.11.7"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
+
+
 libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
-  specs2 % Test
+  specs2 % Test,
+  "com.typesafe.play" %% "anorm" % "2.4.0"
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
